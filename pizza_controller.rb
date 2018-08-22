9018f245b007555c9f84374139d808c5246fcb25
MichaelTheTall <michaeljhnewman@gmail.com>
2018-08-22 13:39:06 +0100

    InittiCommitti

---
+++ pizza_controller.rb
@@ -0,0 +1,9 @@
+require( 'sinatra' )
+require( 'sinatra/contrib/all' )
+require( 'pry-byebug' )
+require_relative( './models/pizza_order' )
+also_reload( './models/*' )
+
+get '/pizza-orders' do
+  erb(:index)
+end
